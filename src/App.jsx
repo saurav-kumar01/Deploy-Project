127d3022333b420afd5e9791e54909991ba395a7
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-07-21 21:26:50 +0530

    change Main file

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
+            Manage Your Todos by Anuj Kumar from Bulandshahr
           </h1>
           <div className="mb-4">{/* Todo form goes here */}
             <TodoForm/>
